20ceab62345004a39b38104782df0bd1490e0e0b
Thomas Schreiner <t.schreiner@gmail.com>
2019-05-17 21:04:51 +0200

    feat: Add tests for value and cell

---
+++ CsvDiff.Tests/TestDiffResultCell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CsvDiff.Tests
+{
+    public class TestDiffResultCell
+    {
+        [Fact]
+        public void ContructorShouldFillAllProperties()
+        {
+            //Arrange
+            var left = new DiffResultValues("a", "a");
+            var right = new DiffResultValues("a", "a");
+
+            //Act
+            var target = new DiffResultCell(left, right, true);
+
+            //Assert
+            Assert.Equal(left, target.Left);
+            Assert.Equal(right, target.Right);
+            Assert.True(target.IsMatch);
+        }
+    }
+}
---
+++ CsvDiff.Tests/TestDiffResultValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CsvDiff.Tests
+{
+    public class TestDiffResultValues
+    {
+        [Fact]
+        public void ConstructorShouldFillPrperties()
+        {
+            //Arrange
+            string original = "A";
+            string processed = "a";
+
+            //Act
+            var target = new DiffResultValues(original, processed);
+
+            //Assert
+            Assert.Equal(original, target.Original);
+            Assert.Equal(processed, target.Processed);
+        }
+    }
+}
